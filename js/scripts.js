1486564f419aac327fe074c7b72632572ef4d988
Patryk-Wawa <nadawcamaila@o2.pl>
2017-07-31 19:30:10 +0200

    spacje plus html

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@
 
 var a = 3;
 var h = 5;
-var triangleArea = a*h/2;
+var triangleArea = a * h / 2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
